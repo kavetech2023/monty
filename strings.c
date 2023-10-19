0e25681836994bf61e8e1894459fe06f21359afa
kavetech2023 <137928651+kavetech2023@users.noreply.github.com>
2023-10-19 19:59:10 +0300

    Update strings.c

--- strings.c
+++ strings.c
@@ -4,6 +4,10 @@
  * print_char - Prints the ASCII character represented by the top element of the stack.
  * @stack: Pointer to a pointer pointing to the top node of the stack.
  * @line_number: Integer representing the line number of the opcode.
+ *
+ * This function checks if the stack is not empty and the top element's value is a valid
+ * ASCII value (between 0 and 127). If so, it prints the corresponding character.
+ * If the stack is empty or the value is out of range, it triggers an error.
  */
 void print_char(stack_t **stack, unsigned int line_number)
 {
@@ -21,6 +25,10 @@ void print_char(stack_t **stack, unsigned int line_number)
  * print_str - Prints a string by interpreting the stack's elements as ASCII characters.
  * @stack: Pointer to a pointer pointing to the top node of the stack.
  * @line_number: Integer representing the line number of the opcode.
+ *
+ * This function prints a sequence of characters by interpreting the stack's elements
+ * as ASCII values. It continues to print characters until an element is not a valid
+ * ASCII value (outside the range of 0 to 127) or the stack is empty.
  */
 void print_str(stack_t **stack, unsigned int line_number)
 {
@@ -48,6 +56,10 @@ void print_str(stack_t **stack, unsigned int line_number)
  * rotl - Rotates the first node of the stack to the bottom.
  * @stack: Pointer to a pointer pointing to the top node of the stack.
  * @line_number: Integer representing the line number of the opcode.
+ *
+ * This function rotates the first node of the stack to the bottom, maintaining
+ * the order of the other nodes. If the stack is empty or has only one element,
+ * it does nothing.
  */
 void rotl(stack_t **stack, unsigned int line_number)
 {
@@ -69,6 +81,10 @@ void rotl(stack_t **stack, unsigned int line_number)
  * rotr - Rotates the last node of the stack to the top.
  * @stack: Pointer to a pointer pointing to the top node of the stack.
  * @line_number: Integer representing the line number of the opcode.
+ *
+ * This function rotates the last node of the stack to the top, preserving
+ * the order of the other nodes. If the stack is empty or has only one element,
+ * it does nothing.
  */
 void rotr(stack_t **stack, unsigned int line_number)
 {
@@ -83,5 +99,5 @@ void rotr(stack_t **stack, unsigned int line_number)
     tmp->prev->next = NULL;
     tmp->prev = NULL;
     (*stack)->prev = tmp;
-    *stack = tmp;
+    (*stack) = tmp;
 }
